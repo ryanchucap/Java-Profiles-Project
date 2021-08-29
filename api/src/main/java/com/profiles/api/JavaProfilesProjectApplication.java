e5f0b0d88280597dc501442226ff150d06bff2a1
Chu <ryan.chu@capgemini.com>
2021-08-29 16:00:43 -0400

    api commit

---
+++ api/src/main/java/com/profiles/api/JavaProfilesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.profiles.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProfilesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProfilesProjectApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/profiles/api/JavaProfilesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profiles.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProfilesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
